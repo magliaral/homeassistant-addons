cf52da1076e11f0b7f6248dd15b89710aa78d7ce
Alessio Magliarella <49778555+magliaral@users.noreply.github.com>
2025-08-11 21:49:17 +0200

    Delete bacnet_hub directory

--- bacnet_hub/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [ -z "${SUPERVISOR_TOKEN}" ]; then
-  echo "ERROR: SUPERVISOR_TOKEN not set. Start this as a Home Assistant add-on."
-  exit 1
-fi
-
-exec python3 /usr/src/app/server.py
