252200dbe86b0bfb0325fd6b3396480f5a9fb342
Alessio Magliarella <49778555+magliaral@users.noreply.github.com>
2025-08-13 13:51:08 +0200

    Update run.sh

--- bacnet-hub/run.sh
+++ bacnet-hub/run.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-TZ_VALUE=$(bashio::config 'TZ')
-if [ -z "$TZ_VALUE" ]; then
-    TZ_VALUE="Europe/Zurich"
-fi
-
 if [ -e "/usr/share/zoneinfo/$TZ_VALUE" ]; then
     ln -snf "/usr/share/zoneinfo/$TZ_VALUE" /etc/localtime
     echo "$TZ_VALUE" > /etc/timezone
